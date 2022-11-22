6d43da1e77443eb4b9878f3c06ce3f5638e866ff
jmamatov <115724519+jmamatov@users.noreply.github.com>
2022-11-22 19:32:10 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+//?String
+
+
+let name = "Mening ismim Jahongir"
+let surname = "Familiyam Mamatov" 
+var young = "Yoshim  15 da"
+let accommodation = "Turar joyim Do'nqorg'on"
+var yearofbirth = "Tug'ilgan yilim 2010"
+
+
+console.log(name);
+console.log(surname);
+console.log(young);
+console.log(yearofbirth);
+
+
+//?String concatenatoin
+
+var location = "O'qish joyi"
+var Placeofstudy ="Piskent tumani "
+var school = "19-Maktab"
+let full = Placeofstudy + ' ' + school
+
+console.log(location, full);
